f6eb3369a3d3472c529545e086ca2aae1e0778f7
Mauricio Foronda <josemauricioforonda@gmail.com>
2020-04-20 18:30:35 -0400

    init

---
+++ scripts/update.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import requests
+import pandas as pd
+from io import StringIO
+from datetime import datetime, timedelta
+
+def update():
+  url = 'https://juliael.carto.com/api/v2/sql?filename=capital_municipios_covid19&q=SELECT+*+FROM+(select+*+from+public.capital_municipios_covid19)+as+subq+&format=csv&bounds=&api_key=&skipfields=the_geom_webmercator'
+  yesterday = (datetime.today()-timedelta(days=1)).strftime('%Y-%m-%d')
+  filename = 'data/{}.csv'.format(yesterday)
+  response = requests.get(url).content
+  df = pd.read_csv(StringIO(response.decode('utf-8')))
+  df[['positivos', 'recuperados', 'decesos']] = df[['positivos', 'recuperados', 'decesos']].fillna(0).apply(lambda x: pd.to_numeric(x, downcast='integer'))
+  df[['municipio', 'positivos', 'recuperados','decesos']].to_csv(filename, index=False)
+  print(yesterday)
+
+update()
