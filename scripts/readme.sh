6204655ebbe171284762776b52206d1bec619d30
Mauricio Foronda <josemauricioforonda@gmail.com>
2020-04-22 12:13:46 -0400

    workflow fix

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -2,4 +2,4 @@
 days=( $(ls -r data | head -n 2) )
 echo -e "Datos de casos de covid19 en Bolivia por municipio, reportados diariamente.\n\nCambios en el último día:\n\n" > readme.md
 daff --padding sparse --context 0 --unordered --output-format html --fragment "data/${days[1]}" "data/${days[0]}">> readme.md
-echo -e "\n+++ : nuevo municipio\n\n→ : nuevos casos" >> readme.md
+echo -e "\n+++ : nuevo municipio\n\n→ : nuevos casos\nActualizo los datos a medio día (GMT-4) para el día anterior." >> readme.md
