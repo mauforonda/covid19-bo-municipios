610b1b73d95a14301bcf421d4b9d420e1856963d
Mauricio Foronda <josemauricioforonda@gmail.com>
2020-05-02 11:48:53 -0400

    handle situations when they forget to update data
    checks every hour from 12 to 18 (GMT-4) and only updates when there is new information

--- scripts/update.py
+++ scripts/update.py
@@ -4,15 +4,27 @@ import requests
 import pandas as pd
 from io import StringIO
 from datetime import datetime, timedelta
+import os
 
 def update():
+  
+  # Get the latest data
   url = 'https://juliael.carto.com/api/v2/sql?filename=capital_municipios_covid19&q=SELECT+*+FROM+(select+*+from+public.capital_municipios_covid19)+as+subq+&format=csv&bounds=&api_key=&skipfields=the_geom_webmercator'
   yesterday = (datetime.today()-timedelta(days=1)).strftime('%Y-%m-%d')
   filename = 'data/{}.csv'.format(yesterday)
   response = requests.get(url).content
   df = pd.read_csv(StringIO(response.decode('utf-8')))
   df[['cod_ut', 'positivos', 'recuperados', 'decesos']] = df[['cod_ut', 'positivos', 'recuperados', 'decesos']].fillna(0).apply(lambda x: pd.to_numeric(x, downcast='integer'))
-  df[['municipio', 'cod_ut', 'positivos', 'recuperados','decesos']].to_csv(filename, index=False, header=['municipio', 'cod_ine', 'confirmados', 'recuperados', 'decesos'])
-  print(yesterday)
+  
+  # Check how many positive there were yesterday
+  last_day = sorted(os.listdir('data'))[-1]
+  last_day_total = pd.read_csv('data/{}'.format(last_day))['confirmados'].sum()
+  
+  # Only write to file if there is a difference
+  if last_day_total != df['positivos'].sum():
+    df[['municipio', 'cod_ut', 'positivos', 'recuperados','decesos']].to_csv(filename, index=False, header=['municipio', 'cod_ine', 'confirmados', 'recuperados', 'decesos'])
+    print(yesterday)
+  else:
+    print('Nop')
 
 update()
